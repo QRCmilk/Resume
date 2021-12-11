c2264b1cafef87046e113a0d25dce0618beb12ac
QRCmilk <92916423+QRCmilk@users.noreply.github.com>
2021-12-11 22:43:36 +0800

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,349 @@
+function a()
+{
+
+    if(document.getElementById('open1').style.display == "block")
+    {
+        document.getElementById('open1').style.display = "none";
+    }
+    else
+    {
+        document.getElementById('open1').style.display = "block";
+    }
+    openword();
+    openppt();
+    openexcelt();
+    openaccesst();
+}
+function b()
+{
+    if(document.getElementById('open2').style.display == "none")
+    {
+        document.getElementById('open2').style.display = "block";
+    }
+    else
+    {
+        document.getElementById('open2').style.display = "none";
+    }
+    openhtmlt();
+    opencsst();
+    openjst();
+    openjavat();
+    openct();
+}
+function exp1()
+{
+
+    if(document.getElementById('exp1').style.display == "block")
+    {
+        document.getElementById('exp1').style.display = "none";
+        document.getElementById('exp1-2').style.display = "none";
+        document.getElementById('exp2').style.display = "none";
+        document.getElementById('exp2-2').style.display = "none";
+        document.getElementById('exp3').style.display = "none";
+        document.getElementById('exp3-2').style.display = "none";
+    }
+    else
+    {
+        document.getElementById('exp1').style.display = "block";
+        document.getElementById('exp1-2').style.display = "block";
+        document.getElementById('exp2').style.display = "none";
+        document.getElementById('exp2-2').style.display = "none";
+        document.getElementById('exp3').style.display = "none";
+        document.getElementById('exp3-2').style.display = "none";
+    }
+}
+function exp2()
+{
+
+    if(document.getElementById('exp2').style.display == "block")
+    {
+        document.getElementById('exp2').style.display = "none";
+        document.getElementById('exp2-2').style.display = "none";
+        document.getElementById('exp1').style.display = "none";
+        document.getElementById('exp1-2').style.display = "none";
+        document.getElementById('exp3').style.display = "none";
+        document.getElementById('exp3-2').style.display = "none";
+    }
+    else
+    {
+        document.getElementById('exp2').style.display = "block";
+        document.getElementById('exp2-2').style.display = "block";
+        document.getElementById('exp1').style.display = "none";
+        document.getElementById('exp1-2').style.display = "none";
+        document.getElementById('exp3').style.display = "none";
+        document.getElementById('exp3-2').style.display = "none";
+    }
+}
+function exp3()
+{
+
+    if(document.getElementById('exp3').style.display == "block")
+    {
+        document.getElementById('exp3').style.display = "none";
+        document.getElementById('exp3-2').style.display = "none";
+        document.getElementById('exp1').style.display = "none";
+        document.getElementById('exp1-2').style.display = "none";
+        document.getElementById('exp2').style.display = "none";
+        document.getElementById('exp2-2').style.display = "none";
+    }
+    else
+    {
+        document.getElementById('exp3').style.display = "block";
+        document.getElementById('exp3-2').style.display = "block";
+        document.getElementById('exp2').style.display = "none";
+        document.getElementById('exp2-2').style.display = "none";
+        document.getElementById('exp1').style.display = "none";
+        document.getElementById('exp1-2').style.display = "none";
+    }
+}
+function exp4()
+{
+
+    if(document.getElementById('exp4').style.display == "block")
+    {
+        document.getElementById('exp4').style.display = "none";
+        document.getElementById('exp4-2').style.display = "none";
+        document.getElementById('exp5').style.display = "none";
+        document.getElementById('exp5-2').style.display = "none";
+        document.getElementById('exp6').style.display = "none";
+        document.getElementById('exp6-2').style.display = "none";
+    }
+    else
+    {
+        document.getElementById('exp4').style.display = "block";
+        document.getElementById('exp4-2').style.display = "block";
+        document.getElementById('exp6').style.display = "none";
+        document.getElementById('exp6-2').style.display = "none";
+        document.getElementById('exp5').style.display = "none";
+        document.getElementById('exp5-2').style.display = "none";
+    }
+}
+function exp5()
+{
+
+    if(document.getElementById('exp5').style.display == "block")
+    {
+        document.getElementById('exp5').style.display = "none";
+        document.getElementById('exp5-2').style.display = "none";
+        document.getElementById('exp4').style.display = "none";
+        document.getElementById('exp4-2').style.display = "none";
+        document.getElementById('exp6').style.display = "none";
+        document.getElementById('exp6-2').style.display = "none";
+    }
+    else
+    {
+        document.getElementById('exp5').style.display = "block";
+        document.getElementById('exp5-2').style.display = "block";
+        document.getElementById('exp6').style.display = "none";
+        document.getElementById('exp6-2').style.display = "none";
+        document.getElementById('exp4').style.display = "none";
+        document.getElementById('exp4-2').style.display = "none";
+    }
+}
+function exp6()
+{
+
+    if(document.getElementById('exp6').style.display == "block")
+    {
+        document.getElementById('exp6').style.display = "none";
+        document.getElementById('exp6-2').style.display = "none";
+        document.getElementById('exp4').style.display = "none";
+        document.getElementById('exp4-2').style.display = "none";
+        document.getElementById('exp5').style.display = "none";
+        document.getElementById('exp5-2').style.display = "none";
+    }
+    else
+    {
+        document.getElementById('exp6').style.display = "block";
+        document.getElementById('exp6-2').style.display = "block";
+        document.getElementById('exp5').style.display = "none";
+        document.getElementById('exp5-2').style.display = "none";
+        document.getElementById('exp4').style.display = "none";
+        document.getElementById('exp4-2').style.display = "none";
+    }
+}
+function exp7()
+{
+
+    if(document.getElementById('exp7').style.display == "block")
+    {
+        document.getElementById('exp7').style.display = "none";
+        document.getElementById('exp7-2').style.display = "none";
+        document.getElementById('exp8').style.display = "none";
+        document.getElementById('exp8-2').style.display = "none";
+        document.getElementById('exp9').style.display = "none";
+        document.getElementById('exp9-2').style.display = "none";
+    }
+    else
+    {
+        document.getElementById('exp7').style.display = "block";
+        document.getElementById('exp7-2').style.display = "block";
+        document.getElementById('exp9').style.display = "none";
+        document.getElementById('exp9-2').style.display = "none";
+        document.getElementById('exp8').style.display = "none";
+        document.getElementById('exp8-2').style.display = "none";
+    }
+}
+function exp8()
+{
+
+    if(document.getElementById('exp8').style.display == "block")
+    {
+        document.getElementById('exp8').style.display = "none";
+        document.getElementById('exp8-2').style.display = "none";
+        document.getElementById('exp7').style.display = "none";
+        document.getElementById('exp7-2').style.display = "none";
+        document.getElementById('exp9').style.display = "none";
+        document.getElementById('exp9-2').style.display = "none";
+    }
+    else
+    {
+        document.getElementById('exp8').style.display = "block";
+        document.getElementById('exp8-2').style.display = "block";
+        document.getElementById('exp9').style.display = "none";
+        document.getElementById('exp9-2').style.display = "none";
+        document.getElementById('exp7').style.display = "none";
+        document.getElementById('exp7-2').style.display = "none";
+    }
+}
+function exp9()
+{
+
+    if(document.getElementById('exp9').style.display == "block")
+    {
+        document.getElementById('exp9').style.display = "none";
+        document.getElementById('exp9-2').style.display = "none";
+        document.getElementById('exp8').style.display = "none";
+        document.getElementById('exp8-2').style.display = "none";
+        document.getElementById('exp7').style.display = "none";
+        document.getElementById('exp7-2').style.display = "none";
+    }
+    else
+    {
+        document.getElementById('exp9').style.display = "block";
+        document.getElementById('exp9-2').style.display = "block";
+        document.getElementById('exp7').style.display = "none";
+        document.getElementById('exp7-2').style.display = "none";
+        document.getElementById('exp8').style.display = "none";
+        document.getElementById('exp8-2').style.display = "none";
+    }
+}
+function openword(){
+    var mod = document.getElementById("wordt");
+    var times = 0;
+
+    var startNum= setInterval(function(){
+        times+=1;
+        mod.innerHTML = times + "%";
+        if(times==90){
+            clearInterval(startNum);
+        
+        }
+    },22.5)
+}
+function openppt(){
+    var mod = document.getElementById("pptt");
+    var times = 0;
+
+    var startNum= setInterval(function(){
+        times+=1;
+        mod.innerHTML = times + "%";
+        if(times==80){
+            clearInterval(startNum);
+        
+        }
+    },25)
+}
+function openexcelt(){
+    var mod = document.getElementById("excelt");
+    var times = 0;
+
+    var startNum= setInterval(function(){
+        times+=1;
+        mod.innerHTML = times + "%";
+        if(times==75){
+            clearInterval(startNum);
+        
+        }
+    },26.25)
+}
+function openaccesst(){
+    var mod = document.getElementById("accesst");
+    var times = 0;
+
+    var startNum= setInterval(function(){
+        times+=1;
+        mod.innerHTML = times + "%";
+        if(times==55){
+            clearInterval(startNum);
+        
+        }
+    },36.25)
+}
+function openhtmlt(){
+    var mod = document.getElementById("htmlt");
+    var times = 0;
+
+    var startNum= setInterval(function(){
+        times+=1;
+        mod.innerHTML = times + "%";
+        if(times==90){
+            clearInterval(startNum);
+        
+        }
+    },22.5)
+}
+function opencsst(){
+    var mod = document.getElementById("csst");
+    var times = 0;
+
+    var startNum= setInterval(function(){
+        times+=1;
+        mod.innerHTML = times + "%";
+        if(times==80){
+            clearInterval(startNum);
+        
+        }
+    },25)
+}
+function openjst(){
+    var mod = document.getElementById("jst");
+    var times = 0;
+
+    var startNum= setInterval(function(){
+        times+=1;
+        mod.innerHTML = times + "%";
+        if(times==55){
+            clearInterval(startNum);
+        
+        }
+    },36.25)
+}
+function openjavat(){
+    var mod = document.getElementById("javat");
+    var times = 0;
+
+    var startNum= setInterval(function(){
+        times+=1;
+        mod.innerHTML = times + "%";
+        if(times==75){
+            clearInterval(startNum);
+        
+        }
+    },26.25)
+}
+function openct(){
+    var mod = document.getElementById("ct");
+    var times = 0;
+
+    var startNum= setInterval(function(){
+        times+=1;
+        mod.innerHTML = times + "%";
+        if(times==70){
+            clearInterval(startNum);
+        
+        }
+    },28)
+}
+console.log(times);
+
